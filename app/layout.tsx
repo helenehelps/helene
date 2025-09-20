251617c3f25fe2c938a45a3c3b748f55c9025138
James Aprosail <aprosail@outlook.com>
2025-09-20 08:50:28 +0800

    chore: setup next app

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from "next"
+import { ReactNode } from "react"
+
+export const metadata: Metadata = {
+  title: "Helene",
+  description: "Helene helps you to focus on your idea flow.",
+}
+
+export default function ({ children }: Readonly<{ children: ReactNode }>) {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  )
+}
