41d07d3ada7986a5165894b2a1a4d5e59e2bb08c
James Aprosail <aprosail@outlook.com>
2025-09-20 11:28:31 +0800

    chore: setup cargo crates

---
+++ optimize/helene-server/src/main.rs
@@ -0,0 +1,8 @@
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "it works." }));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
