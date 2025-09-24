8f20937c192fda84dc091772de255599fd366a54
James Aprosail <aprosail@outlook.com>
2025-09-24 21:34:11 +0800

    chore: optimize next config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,23 @@
+import declareScssModules from "@scripts/scss-module-dts"
 import { NextConfig } from "next"
 import createNextIntlPlugin from "next-intl/plugin"
 
-const nextConfig: NextConfig = {}
+function withPlugins(
+  config: NextConfig & {
+    preprocesses?: (() => void)[]
+    functionPlugins?: ((nextConfig: NextConfig) => NextConfig)[]
+  },
+) {
+  const { preprocesses, functionPlugins, ...rest } = config
+  let nextConfig = rest
+  const plugins = functionPlugins || []
+  for (const plugin of plugins) nextConfig = plugin(nextConfig)
+  for (const preprocess of preprocesses || []) preprocess()
+  return nextConfig
+}
 
-export default createNextIntlPlugin("./app/lang.request.ts")(nextConfig)
+// export default createNextIntlPlugin("./app/lang.request.ts")(nextConfig)
+export default withPlugins({
+  preprocesses: [declareScssModules],
+  functionPlugins: [createNextIntlPlugin("./app/lang.request.ts")],
+})
